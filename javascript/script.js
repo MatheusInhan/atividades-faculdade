b336e09e7575c88f3e6631056744c7b150d4a0ab
MatheusInhan <matheusinhan03@gmail.com>
2023-05-08 14:23:39 -0400

    initial commit

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+v1 = 'luiz'
+v2 = 'sub12'
+
+
+console.log(v1 +' gosta de ' + v2)
---
+++ sei la.py
@@ -0,0 +1,11 @@
+n1 = float(input('digite a nota do primeiro bimestre:'))
+n2 = float(input('digite a nota do segundo semestre:'))
+Sm = (n1 + n2)/2 
+
+if Sm>6 :
+    print('você passou')
+    
+else: 
+    print('você não passou')  
+
+print(f'sua media final é {Sm}')    
